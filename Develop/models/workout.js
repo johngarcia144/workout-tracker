6979f19d88e21cdd4516cba7ee216135b177c193
John Garcia <john.garcia144@gmail.com>
2020-10-23 13:27:06 -0600

    edited server and workout

---
+++ Develop/models/workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const workoutSchema = newSchema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {type: String, trim: true},
+        name: {type: String, trim: true},
+        duration: {type: Number},
+        distance: {type: Number},
+        weight: {type: Number},
+        reps: {type: Number},
+        sets: {type: Number}
+    }]
+},
+{
+    toJSON: {
+        // include any virtual properties when data is requested
+        virtuals: true
+      }
+}
+)
---
+++ Develop/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+
+const app = express();
+
+// const databaseName = "workoutdb"
+
+// app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+// Requiring our routes
+
+app.use("/api", require("./routes/api-routes.js"));
+app.use("/", require("./routes/html-routes.js"));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
